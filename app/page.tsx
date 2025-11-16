a4924bf0e313e932e8abe3732218a5057843cf8c
Prithwijit Ghosh <prith.tiu13@gmail.com>
2025-11-16 09:34:39 +0530

    Missed a text

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,27 @@
-import DashboardLayout from "@/components/DashboardLayout";
-import Filters from "@/components/Filters";
-import { OverviewCard } from "@/components/OverviewCard";
-import ProgressOverTime from "@/components/ProgressOverTime";
-import LeadsSummary from "@/components/LeadsSummary";
+import DashboardLayout from '@/components/DashboardLayout';
+import Filters from '@/components/Filters'; 
+import { OverviewCard } from '@/components/OverviewCard';
+import ProgressOverTime from '@/components/ProgressOverTime'; 
+import LeadsSummary from '@/components/LeadsSummary';
 
 export default function Page() {
   return (
     <DashboardLayout>
       <Filters />
-
+      
       <div className="mb-8">
-        <h2 className="text-xl font-semibold mb-4 sr-only">Overview</h2>
-
+        <h2 className="text-xl font-semibold mb-4 text-gray-900">Overview</h2>
+        
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
-          <OverviewCard
-            title="People contacted"
-            value="12000"
-            icon="/overview.png"
-          />
-          <OverviewCard
-            title="No of Leads Generated"
-            value="48"
-            icon="/leads.png"
-          />
-          <OverviewCard
-            title="Sequence in progress"
-            value="234"
-            icon="/sequence.png"
-          />
+          <OverviewCard title="People contacted" value="12000" icon="/overview.png" />
+          <OverviewCard title="No of Leads Generated" value="48" icon="/leads.png" />
+          <OverviewCard title="Sequence in progress" value="234" icon="/sequence.png" />
           <OverviewCard title="Lead rate" value="12%" icon="/lead.png" />
-          <OverviewCard
-            title="Sequence success rate"
-            value="27%"
-            icon="/ss.png"
-          />
+          <OverviewCard title="Sequence success rate" value="27%" icon="/ss.png" />
           <OverviewCard title="New Leads Today" value="56" icon="/leads.png" />
         </div>
       </div>
-
+      
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <ProgressOverTime />
         <LeadsSummary />
